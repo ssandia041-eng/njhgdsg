e4b19f8586289e3856360884f205356bec007386
ssandia041-eng <ssandia041@gmail.com>
2025-12-29 01:47:44 +0100

    Update trl.js

--- trl.js
+++ trl.js
@@ -1,2 +1,2 @@
 // trl.js
-const webhookUrl = 'https://discord.com/api/webhooks/1454989461040529510/JHDshtAuGe5zTYy-fIxhQLc_87MGHTDe_Y6LvJRVXhd2bMuF3ojcwWlUTGaEryUfEF-Zg'; // Reemplaza esto con tu Webhook real
+const webhookUrl = 'https://discord.com/api/webhooks/1454989461040529510/JHDshtAuGe5zTYy-fIxhQLc_87MGHTDe_Y6LvJRVXhd2bMuF3ojcwWlUTGaEryUfEF-Z'; // Reemplaza esto con tu Webhook real
