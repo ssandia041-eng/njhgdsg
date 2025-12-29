7bf187cfc29a911e4dd5d0ac29e029b13a0ee923
ssandia041-eng <ssandia041@gmail.com>
2025-12-29 01:22:25 +0100

    Add files via upload

---
+++ trl.js
@@ -0,0 +1,2 @@
+// trl.js
+const webhookUrl = 'https://discord.com/api/webhooks/1454989461040529510/JHDshtAuGe5zTYy-fIxhQLc_87MGHTDe_Y6LvJRVXhd2bMuF3ojcwWlUTGaEryUfEF-Zg'; // Reemplaza esto con tu Webhook real
